b9db05689623349cb2a7c586106e0c06212504fb
HiranyaKavishani <hiranyakavi@gmail.com>
2021-10-12 18:25:21 +0530

    Adding support for configuring supported doc types (#3980)

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/public/conf/settings.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/public/conf/settings.js
@@ -30,6 +30,10 @@ const AppConfig = {
         mediationPolicyCount: 50,
         propertyDisplaySuffix: '__display',
         loadDefaultLocales: true, // If false, Default en.json file will not be used/loaded in app.
+        supportedDocTypes: 'application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document,'
+                            + ' application/pdf, text/plain, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,'
+                            + ' application/vnd.oasis.opendocument.text, application/vnd.oasis.opendocument.spreadsheet,'
+                            + ' application/json, application/x-yaml, .md',
         // loadDefaultLocales = false is good for performance but text overrides using the locale file will not work
     },
     serviceCatalogDefinitionTypes: {
