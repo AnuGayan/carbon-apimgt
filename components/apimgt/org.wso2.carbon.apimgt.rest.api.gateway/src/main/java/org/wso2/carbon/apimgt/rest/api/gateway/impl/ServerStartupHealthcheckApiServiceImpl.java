adbf143d3987891a83f71c21095558eb67f663c2
mushthaq33 <mushthaq33@gmail.com>
2021-10-28 12:00:43 +0530

    Adding license header

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.gateway/src/main/java/org/wso2/carbon/apimgt/rest/api/gateway/impl/ServerStartupHealthcheckApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.gateway/src/main/java/org/wso2/carbon/apimgt/rest/api/gateway/impl/ServerStartupHealthcheckApiServiceImpl.java
@@ -1,19 +1,28 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.apimgt.rest.api.gateway.impl;
 
-import org.wso2.carbon.apimgt.gateway.utils.GatewayUtils;
-import org.wso2.carbon.apimgt.rest.api.gateway.*;
-import org.wso2.carbon.apimgt.rest.api.gateway.dto.*;
-
-import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.MessageContext;
-
-
-import java.util.List;
-
-import java.io.InputStream;
+import org.wso2.carbon.apimgt.gateway.utils.GatewayUtils;
+import org.wso2.carbon.apimgt.rest.api.gateway.ServerStartupHealthcheckApiService;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
 
 public class ServerStartupHealthcheckApiServiceImpl implements ServerStartupHealthcheckApiService {
