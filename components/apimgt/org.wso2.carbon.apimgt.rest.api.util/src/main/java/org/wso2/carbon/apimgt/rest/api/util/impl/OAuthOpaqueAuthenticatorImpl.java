c521ceebb0ea33b3c37e73ad96435c399f000c70
Sarangan0219 <sarangan@wso2.com>
2022-06-30 08:41:01 +0530

    Adding extension for restAPI authentication
    Increase MAX_LENGTH_PROVIDER and context length
    
    [WSO2 Release] [Jenkins #502] [Release 9.0.174.185] prepare release v9.0.174.185
    
    [WSO2 Release] [Jenkins #502] [Release 9.0.174.185] prepare for next development iteration
    
    Update pom.xml
    
    Refine validate scopes flow
    
    Refine methods to abstact classes
    
    Add extension profiles in Rest API Commons
    
    Remove Store API

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/impl/OAuthOpaqueAuthenticatorImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/impl/OAuthOpaqueAuthenticatorImpl.java
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -21,15 +21,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.cxf.message.Message;
 import org.wso2.carbon.apimgt.api.APIManagementException;
-import org.wso2.carbon.apimgt.api.model.*;
+import org.wso2.carbon.apimgt.api.model.AccessTokenInfo;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.RESTAPICacheConfiguration;
-import org.wso2.carbon.apimgt.impl.caching.CacheProvider;
-import org.wso2.carbon.apimgt.impl.dto.APIKeyValidationInfoDTO;
 import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import org.wso2.carbon.apimgt.rest.api.common.RestApiConstants;
-import org.wso2.carbon.apimgt.rest.api.util.authenticators.WebAppAuthenticator;
 import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.identity.oauth2.OAuth2TokenValidationService;
@@ -41,18 +38,17 @@ import org.wso2.carbon.user.core.service.RealmService;
 import org.wso2.carbon.utils.CarbonUtils;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
-import org.wso2.uri.template.URITemplateException;
+import org.wso2.carbon.apimgt.rest.api.util.authenticators.AbstractOAuthAuthenticator;
 
-import java.util.*;
-import javax.cache.Cache;
+import java.util.Arrays;
 
 /**
- * This web app authenticator class specifically implemented for API Manager store and publisher rest APIs
- * This will not be able to use as generic authenticator.
+ * This OAuthOpaqueAuthenticatorImpl class specifically implemented for API Manager store and publisher rest APIs'
+ * opaque token based authentication
  */
-public class WebAppAuthenticatorImpl implements WebAppAuthenticator {
+public class OAuthOpaqueAuthenticatorImpl extends AbstractOAuthAuthenticator {
 
-    private static final Log log = LogFactory.getLog(WebAppAuthenticatorImpl.class);
+    private static final Log log = LogFactory.getLog(OAuthOpaqueAuthenticatorImpl.class);
     private static final String SUPER_TENANT_SUFFIX =
             APIConstants.EMAIL_DOMAIN_SEPARATOR + MultitenantConstants.SUPER_TENANT_DOMAIN_NAME;
 
@@ -61,6 +57,7 @@ public class WebAppAuthenticatorImpl implements WebAppAuthenticator {
      * @return true if authentication was successful else false
      * @throws APIManagementException when error in authentication process
      */
+    @Override
     public boolean authenticate(Message message) throws APIManagementException {
         boolean retrievedFromInvalidTokenCache = false;
         boolean retrievedFromTokenCache = false;
@@ -71,7 +68,7 @@ public class WebAppAuthenticatorImpl implements WebAppAuthenticator {
         RESTAPICacheConfiguration cacheConfiguration = APIUtil.getRESTAPICacheConfig();
         //validate the token from cache if it is enabled
         if (cacheConfiguration.isTokenCacheEnabled()) {
-            tokenInfo = (AccessTokenInfo)getRESTAPITokenCache().get(accessToken);
+            tokenInfo = (AccessTokenInfo) getRESTAPITokenCache().get(accessToken);
             if (tokenInfo != null) {
                 if (isAccessTokenExpired(tokenInfo)) {
                     tokenInfo.setTokenValid(false);
@@ -86,7 +83,7 @@ public class WebAppAuthenticatorImpl implements WebAppAuthenticator {
                 }
             } else {
                 //if the token doesn't exist in the valid token cache, then check it in the invalid token cache
-                tokenInfo = (AccessTokenInfo)getRESTAPIInvalidTokenCache().get(accessToken);
+                tokenInfo = (AccessTokenInfo) getRESTAPIInvalidTokenCache().get(accessToken);
                 if (tokenInfo != null) {
                     retrievedFromInvalidTokenCache = true;
                 }
@@ -156,99 +153,6 @@ public class WebAppAuthenticatorImpl implements WebAppAuthenticator {
             }
         }
         return false;
-    }
-
-    /**
-     * @param message   CXF message to be validate
-     * @param tokenInfo Token information associated with incoming request
-     * @return return true if we found matching scope in resource and token information
-     * else false(means scope validation failed).
-     */
-    private boolean validateScopes(Message message, AccessTokenInfo tokenInfo) {
-        String basePath = (String) message.get(Message.BASE_PATH);
-        // path is obtained from Message.REQUEST_URI instead of Message.PATH_INFO, as Message.PATH_INFO contains
-        // decoded values of request parameters
-        String path = (String) message.get(Message.REQUEST_URI);
-        String verb = (String) message.get(Message.HTTP_REQUEST_METHOD);
-        String resource = path.substring(basePath.length() - 1);
-        String[] scopes = tokenInfo.getScopes();
-
-        String version = (String) message.get(RestApiConstants.API_VERSION);
-
-        //get all the URI templates of the REST API from the base path
-        Set<URITemplate> uriTemplates = RestApiUtil.getURITemplatesForBasePath(basePath + version);
-        if (uriTemplates.isEmpty()) {
-            if (log.isDebugEnabled()) {
-                log.debug("No matching scopes found for request with path: " + basePath
-                        + ". Skipping scope validation.");
-            }
-            return true;
-        }
-
-        for (Object template : uriTemplates.toArray()) {
-            org.wso2.uri.template.URITemplate templateToValidate = null;
-            Map<String, String> var = new HashMap<String, String>();
-            //check scopes with what we have
-            String templateString = ((URITemplate) template).getUriTemplate();
-            try {
-                templateToValidate = new org.wso2.uri.template.URITemplate(templateString);
-            } catch (URITemplateException e) {
-                log.error("Error while creating URI Template object to validate request. Template pattern: " +
-                        templateString, e);
-            }
-            if (templateToValidate != null && templateToValidate.matches(resource, var) && scopes != null
-                    && verb != null && verb.equalsIgnoreCase(((URITemplate) template).getHTTPVerb())) {
-                for (String scope : scopes) {
-                    Scope scp = ((URITemplate) template).getScope();
-                    if (scp != null) {
-                        if (scope.equalsIgnoreCase(scp.getKey())) {
-                            //we found scopes matches
-                            if (log.isDebugEnabled()) {
-                                log.debug("Scope validation successful for access token: " +
-                                        tokenInfo.getAccessToken() + " with scope: " + scp.getKey() +
-                                        " for resource path: " + path + " and verb " + verb);
-                            }
-                            return true;
-                        }
-                    } else if (!((URITemplate) template).retrieveAllScopes().isEmpty()) {
-                        List<Scope> scopesList = ((URITemplate) template).retrieveAllScopes();
-                        for (Scope scpObj : scopesList) {
-                            if (scope.equalsIgnoreCase(scpObj.getKey())) {
-                                //we found scopes matches
-                                if (log.isDebugEnabled()) {
-                                    log.debug("Scope validation successful for access token: " +
-                                            tokenInfo.getAccessToken() + " with scope: " + scpObj.getKey() +
-                                            " for resource path: " + path + " and verb " + verb);
-                                }
-                                return true;
-                            }
-                        }
-                    } else {
-                        if (log.isDebugEnabled()) {
-                            log.debug("Scope not defined in swagger for matching resource " + resource + " and verb "
-                                    + verb + " . So consider as anonymous permission and let request to continue.");
-                        }
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private Cache getRESTAPITokenCache() {
-        return CacheProvider.getRESTAPITokenCache();
-    }
-
-    private Cache getRESTAPIInvalidTokenCache() {
-        return CacheProvider.getRESTAPIInvalidTokenCache();
-    }
-
-    private boolean isAccessTokenExpired (AccessTokenInfo accessTokenInfo) {
-        APIKeyValidationInfoDTO infoDTO = new APIKeyValidationInfoDTO();
-        infoDTO.setValidityPeriod(accessTokenInfo.getValidityPeriod());
-        infoDTO.setIssuedTime(accessTokenInfo.getIssuedTime());
-        return APIUtil.isAccessTokenExpired(infoDTO);
     }
 
     public AccessTokenInfo getTokenMetaData(String accessToken) throws APIManagementException {
