1e658df8ca222a4b752e290668ff582bc365967e
rusirijayodaillesinghe <rusiri@wso2.com>
2022-11-01 16:13:09 +0530

    Delete empty directory named with UUID which contained API and API Product revisions

--- components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/RegistryPersistenceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/RegistryPersistenceImpl.java
@@ -834,6 +834,14 @@ public class RegistryPersistenceImpl implements APIPersistence {
                     registry.delete(apiProviderPath);
                 }
             }
+
+            /*remove revision directory with UUID*/
+            String revisionDirectoryPath = APIConstants.API_REVISION_LOCATION + RegistryConstants.PATH_SEPARATOR +
+                    apiId;
+            if (registry.resourceExists(revisionDirectoryPath)) {
+                registry.delete(revisionDirectoryPath);
+            }
+
             registry.commitTransaction();
             transactionCommitted  = true;
         } catch (RegistryException e) {
@@ -3510,6 +3518,12 @@ public class RegistryPersistenceImpl implements APIPersistence {
                     }
                     registry.delete(productProviderPath);
                 }
+            }
+            /*remove revision directory with UUID*/
+            String revisionDirectoryPath = APIConstants.API_REVISION_LOCATION + RegistryConstants.PATH_SEPARATOR +
+                    apiId;
+            if (registry.resourceExists(revisionDirectoryPath)) {
+                registry.delete(revisionDirectoryPath);
             }
 
         } catch (RegistryException e) {
