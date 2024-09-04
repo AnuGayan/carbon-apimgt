c2a2c214a8fc877c42cabb76df4a5e4b79501449
msm1992 <madusha.sachini@yahoo.com>
2024-09-04 12:35:17 +0530

    Add Ignore Case String check for APIProduct Type

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -926,7 +926,7 @@ public class ApiMgtDAO {
             if (resultSet.next()) {
                 int applicationId = resultSet.getInt("APPLICATION_ID");
                 Application application = getLightweightApplicationById(conn, applicationId);
-                if (APIConstants.API_PRODUCT.equals(resultSet.getString("API_TYPE"))) {
+                if (APIConstants.API_PRODUCT.equalsIgnoreCase(resultSet.getString("API_TYPE"))) {
                     APIProductIdentifier apiProductIdentifier = new APIProductIdentifier(
                             APIUtil.replaceEmailDomain(resultSet.getString("API_PROVIDER")),
                             resultSet.getString("API_NAME"), resultSet.getString("API_VERSION"));
@@ -981,7 +981,7 @@ public class ApiMgtDAO {
             if (resultSet.next()) {
                 Identifier identifier;
 
-                if (APIConstants.API_PRODUCT.equals(resultSet.getString("API_TYPE"))) {
+                if (APIConstants.API_PRODUCT.equalsIgnoreCase(resultSet.getString("API_TYPE"))) {
                     identifier = new APIProductIdentifier(
                             APIUtil.replaceEmailDomain(resultSet.getString("API_PROVIDER")),
                             resultSet.getString("API_NAME"), resultSet.getString("API_VERSION"));
@@ -1542,7 +1542,7 @@ public class ApiMgtDAO {
             while (result.next()) {
                 String apiType = result.getString("TYPE");
 
-                if (APIConstants.API_PRODUCT.toString().equals(apiType)) {
+                if (APIConstants.API_PRODUCT.equalsIgnoreCase(apiType)) {
                     APIProductIdentifier identifier =
                             new APIProductIdentifier(APIUtil.replaceEmailDomain(result.getString("API_PROVIDER")),
                                     result.getString("API_NAME"), result.getString("API_VERSION"));
@@ -9973,7 +9973,7 @@ public class ApiMgtDAO {
                         String context = resultSet.getString("CONTEXT");
                         String apiType = resultSet.getString("API_TYPE");
                         String version = resultSet.getString("API_VERSION");
-                        if (APIConstants.API_PRODUCT.equals(apiType)
+                        if (APIConstants.API_PRODUCT.equalsIgnoreCase(apiType)
                                 && APIConstants.API_PRODUCT_VERSION_1_0_0.equals(version)
                                 && StringUtils.isBlank(contextTemplate)) {
                             context = context + "/" + APIConstants.API_PRODUCT_VERSION_1_0_0;
@@ -16724,7 +16724,7 @@ public class ApiMgtDAO {
                 String contextTemplate = resultSet.getString("CONTEXT_TEMPLATE");
 
                 String uuid = resultSet.getString("API_UUID");
-                if (APIConstants.API_PRODUCT.equals(resultSet.getString("API_TYPE"))) {
+                if (APIConstants.API_PRODUCT.equalsIgnoreCase(resultSet.getString("API_TYPE"))) {
                     // skip api products
                     continue;
                 }
@@ -18889,7 +18889,7 @@ public class ApiMgtDAO {
             try (ResultSet result = ps.executeQuery()) {
                 while (result.next()) {
                     String apiType = result.getString("TYPE");
-                    if (!APIConstants.API_PRODUCT.toString().equals(apiType)) {
+                    if (!APIConstants.API_PRODUCT.equalsIgnoreCase(apiType)) {
                         APIIdentifier identifier = new APIIdentifier(APIUtil.replaceEmailDomain(result.getString
                                 ("API_PROVIDER")), result.getString("API_NAME"),
                                 result.getString("API_VERSION"));
@@ -18924,7 +18924,7 @@ public class ApiMgtDAO {
                     if (index >= offset && index < limit) {
                         String apiType = result.getString("TYPE");
 
-                        if (APIConstants.API_PRODUCT.toString().equals(apiType)) {
+                        if (APIConstants.API_PRODUCT.equalsIgnoreCase(apiType)) {
                             APIProductIdentifier identifier = new APIProductIdentifier(
                                     APIUtil.replaceEmailDomain(result.getString("API_PROVIDER")),
                                     result.getString("API_NAME"), result.getString("API_VERSION"));
