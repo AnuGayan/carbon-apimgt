b3f87ac16919cf14d478fe0602d642bf0faa50a5
Tharanidk <tharani.dayadhi@gmail.com>
2024-12-19 12:09:08 +0530

    add missing import

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -287,6 +287,7 @@ import javax.cache.CacheManager;
 import javax.cache.Caching;
 import java.security.cert.X509Certificate;
 import javax.validation.constraints.NotNull;
+import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
