68c14d281229beb41db63be8970a553f93af0d9f
dakshithas <dakshitha.sur@gmail.com>
2025-03-31 12:57:12 +0530

    Fix #9094 : fix INTERNAL role cache for DevPortal API visibility

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -198,6 +198,7 @@ import org.wso2.carbon.governance.api.generic.GenericArtifactManager;
 import org.wso2.carbon.governance.api.generic.dataobjects.GenericArtifact;
 import org.wso2.carbon.governance.api.util.GovernanceUtils;
 import org.wso2.carbon.identity.core.util.IdentityCoreConstants;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth.OAuthAdminService;
 import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
 import org.wso2.carbon.registry.core.ActionConstants;
@@ -2388,6 +2389,10 @@ public final class APIUtil {
             return authorized;
         }
 
+        if (!IdentityUtil.isUserStoreInUsernameCaseSensitive(userNameWithoutChange)) {
+            userNameWithoutChange = userNameWithoutChange.toLowerCase();
+        }
+        
         if (APIConstants.Permissions.APIM_ADMIN.equals(permission) || APIConstants.Permissions.API_CREATE.equals(permission)
                 || APIConstants.Permissions.API_PUBLISH.equals(permission)) {
             String cacheKey = userNameWithoutChange + ":" + permission;
@@ -2477,6 +2482,10 @@ public final class APIUtil {
         if (username == null) {
             throw new APIManagementException("Attempt to execute privileged operation as" +
                     " the anonymous user");
+        }
+
+        if (!IdentityUtil.isUserStoreInUsernameCaseSensitive(username)) {
+            username = username.toLowerCase();
         }
 
         String[] roles = getValueFromCache(APIConstants.API_USER_ROLE_CACHE, username);
