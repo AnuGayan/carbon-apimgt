8b61422b4f8ad1a5e3190c5307bc97c168afc61e
Pasan Tennakoon <pasan96tennakoon@gmail.com>
2025-04-04 17:32:35 +0530

    Clear APIResource cache on undeploy event

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/InMemoryAPIDeployer.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/InMemoryAPIDeployer.java
@@ -45,6 +45,8 @@ import org.wso2.carbon.apimgt.gateway.internal.DataHolder;
 import org.wso2.carbon.apimgt.gateway.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.gateway.service.APIGatewayAdmin;
 import org.wso2.carbon.apimgt.impl.APIConstants;
+import org.wso2.carbon.apimgt.impl.APIManagerConfiguration;
+import org.wso2.carbon.apimgt.impl.caching.CacheInvalidationServiceImpl;
 import org.wso2.carbon.apimgt.impl.dto.ExtendedJWTConfigurationDto;
 import org.wso2.carbon.apimgt.impl.dto.GatewayArtifactSynchronizerProperties;
 import org.wso2.carbon.apimgt.impl.dto.GatewayCleanupSkipList;
@@ -362,6 +364,13 @@ public class InMemoryAPIDeployer {
                 apiGatewayAdmin.unDeployAPI(gatewayAPIDTO);
                 DataHolder.getInstance().getApiToCertificatesMap().remove(gatewayEvent.getUuid());
                 DataHolder.getInstance().removeKeyManagerToAPIMapping(gatewayAPIDTO.getApiId());
+                if (isAPIResourceValidationEnabled()) {
+                    new CacheInvalidationServiceImpl().invalidateResourceCache(
+                            gatewayEvent.getContext(),
+                            gatewayEvent.getVersion(),
+                            gatewayEvent.getTenantDomain(),
+                            new ArrayList<>());
+                }
             }
     }
 
@@ -376,6 +385,22 @@ public class InMemoryAPIDeployer {
         } finally {
             MessageContext.destroyCurrentMessageContext();
         }
+    }
+
+    /**
+     * Check whether the API resource validation is enabled or not.
+     *
+     * @return true if enabled, false otherwise
+     */
+    public boolean isAPIResourceValidationEnabled() {
+        try {
+            APIManagerConfiguration config = ServiceReferenceHolder.getInstance().getAPIManagerConfiguration();
+            String gatewayResourceCacheEnabled = config.getFirstProperty(APIConstants.GATEWAY_RESOURCE_CACHE_ENABLED);
+            return Boolean.parseBoolean(gatewayResourceCacheEnabled);
+        } catch (Exception e) {
+            log.error("Error occurred while reading Gateway cache configurations. Use default configurations" + e);
+        }
+        return true;
     }
 
     public void cleanDeployment(String artifactRepositoryPath) {
