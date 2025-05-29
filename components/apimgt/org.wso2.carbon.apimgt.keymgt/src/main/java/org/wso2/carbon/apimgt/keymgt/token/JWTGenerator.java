ad17a8b56869d133f081f3ed2b98350c7f20bad5
hisanhunais <hisan.live@gmail.com>
2025-05-29 16:38:26 +0530

    Remove default value of system property

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/token/JWTGenerator.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/token/JWTGenerator.java
@@ -56,8 +56,7 @@ public class JWTGenerator extends AbstractJWTGenerator {
     private static final Log log = LogFactory.getLog(JWTGenerator.class);
     private static final String OIDC_DIALECT_URI = "http://wso2.org/oidc/claim";
     private static final boolean BINDING_FEDERATED_USER_CLAIMS_FOR_OPAQUE =
-            Boolean.parseBoolean(System.getProperty(APIConstants.KeyManager.BINDING_FEDERATED_USER_CLAIMS_FOR_OPAQUE,
-                    "false"));
+            Boolean.parseBoolean(System.getProperty(APIConstants.KeyManager.ENABLE_BIND_FEDERATED_USER_CLAIMS_FOR_OPAQUE));
 
     @Override
     public Map<String, String> populateStandardClaims(TokenValidationContext validationContext)
